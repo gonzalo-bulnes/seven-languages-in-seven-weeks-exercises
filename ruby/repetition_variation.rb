827fad09c1834a6912ed75198e06e9e70e1e4352
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2013-06-28 01:58:24 -0400

    Add repetition_variation

---
+++ ruby/repetition_variation.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/ruby
+
+# Print the string “This is sentence number 1,” where the number 1
+# changes from 1 to 10.
+(1..10).each do |number|
+  puts "This is sentence number #{number}"
+end
