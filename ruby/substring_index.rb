8ce2220bda78f0dc5ce6fd0f8842f4fdd175f1b9
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2013-06-28 01:57:12 -0400

    Add substring_index

---
+++ ruby/substring_index.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/ruby
+
+# For the string “Hello, Ruby,” find the index of the word “Ruby.”
+# I assume that the comma and colon inside the quote marks is a LaTeX effect and
+# that the actual strings are "Hello, Ruby." (colon, not comma) and "Ruby" (no colon).
+# See http://ruby-doc.org/core-2.0/String.html#method-i-index
+puts 'Hello, Ruby.'.index('Ruby')
